a8ef109f562893e57fea344f39be464ae08b7a89
crypto0sss <martynovvad1@icloud.com>
2024-11-13 23:09:08 +0300

    HeapSort UI

--- 3/NumberRepository.java
+++
@@ -1,6 +0,0 @@
-public interface NumberRepository {
-    void add(int number);               // Create
-    int[] getAll();                     // Read
-    void update(int oldNumber, int newNumber); // Update
-    void delete(int number);            // Delete
-}
